f60f1211799fb7e7ae07c211c8bf2d03fecb1e4a
madyardwn <madyardwn@gmail.com>
2024-09-22 13:14:47 +0000

    feat: left over

--- src/utils/winston.ts
+++ src/utils/winston.ts
@@ -19,9 +19,7 @@ const logger = winston.createLogger({
             format: "YYYY-MM-DD hh:mm:ss.SSS A"
         }),
         winston.format.label({ label: "[LOGGER]" }),
-        winston.format.printf(
-            (info) => ` ${info.label} ${info.timestamp} ${info.level} : ${info.message}`
-        )
+        winston.format.printf((info) => ` ${info.label} ${info.timestamp} ${info.level} : ${info.message}`)
     ),
     transports: [
         new winston.transports.Console({
